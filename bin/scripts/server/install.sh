6d47520257c394b59aaa6a76d2239dee2f4a3825
AspieSoft <aspiesoftweb@gmail.com>
2024-03-29 09:10:08 -0400

    updated

--- bin/scripts/server/install.sh
+++ bin/scripts/server/install.sh
@@ -18,7 +18,7 @@ git clone --recurse-submodules https://github.com/fairyglade/ly
 cd ly
 make
 make install installsystemd
-systemctl enable ly.service --now
+systemctl enable ly.service
 systemctl disable getty@tty2.service
 cd ..
 rm -rf ./ly
